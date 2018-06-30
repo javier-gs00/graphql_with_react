028669cdde6674f024cd064c51d3663a01c5f537
Javier Gutierrez <javiergs1106@gmail.com>
2018-06-30 19:03:30 -0400

    Refactor query to fetch songs and update after submit

--- Lyrical-GraphQL/client/components/SongCreate.js
+++ Lyrical-GraphQL/client/components/SongCreate.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import gql from 'graphql-tag'
 import { graphql } from 'react-apollo'
+import { Link, hashHistory } from 'react-router'
+import fetchSongs from '../queries/fetchSongs'
 
 class SongCreate extends Component {
   constructor(props) {
     super(props)
     this.state = { title: '' }
+    this.onSubmit = this.onSubmit.bind(this)
   }
 
   onSubmit(event) {
@@ -14,15 +17,17 @@ class SongCreate extends Component {
     this.props.mutate({
       variables: {
         title: this.state.title
-      }
-    })
+      },
+      refetchQueries: [{ query: fetchSongs }]
+    }).then(() => hashHistory.push('/'))
   }
 
   render() {
     return (
       <div>
+        <Link to="/">Back</Link>
         <h3>Create a New Song</h3>
-        <form onSubmit={this.onSubmit.bind(this)}>
+        <form onSubmit={event => this.onSubmit(event)}>
           <label>Song Title:</label>
           <input
             onChange={event => this.setState({ title: event.target.value })}
