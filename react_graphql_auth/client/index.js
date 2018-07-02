7b7e64244aa04f5fb3680123c9e1403a2ab6eb89
Javier Gutierrez <javiergs1106@gmail.com>
2018-07-02 15:13:23 -0400

    Add react_graphql_auth project

---
+++ react_graphql_auth/client/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-client'
+import { ApolloProvider } from 'react-apollo'
+
+const client = new ApolloClient({
+  dataIdFromObject: o => o.id
+})
+
+const Root = () => {
+  return (
+    <ApolloProvider client={client}>
+      <div>
+        Auth Starter
+      </div>
+    </ApolloProvider>
+  );
+};
+
+ReactDOM.render(<Root />, document.querySelector('#root'));
